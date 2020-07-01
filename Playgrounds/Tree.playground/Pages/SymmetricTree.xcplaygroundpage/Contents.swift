4f2a3113b221bace79fe89ea24b58d40c939eb75
dep2k <deep.mobdev@gmail.com>
2020-07-01 21:35:14 +0530

    Symmetric Tree

---
+++ Playgrounds/Tree.playground/Pages/SymmetricTree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,34 @@
+
+import Foundation
+
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        guard let root
+        return isMirror(root, root)
+    }
+    
+    func isMirror(_ root1: TreeNode?, _ root2: TreeNode?) -> Bool {
+        if root1 == nil && root2 == nil {
+            return true
+        }
+        
+        if (root1 != nil && root2 != nil && root1!.val == root2!.val) {
+            return isMirror(root1!.left, root2!.right) && isMirror(root1!.right, root2!.left)
+        }
+        return false
+    }
+}
