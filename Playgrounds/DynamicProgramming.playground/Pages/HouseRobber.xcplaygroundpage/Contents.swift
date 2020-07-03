f7602a8a5d66c68a75872c026b674d70cc3c0070
dep2k <deep.mobdev@gmail.com>
2020-07-03 14:55:43 +0530

    House Robber

---
+++ Playgrounds/DynamicProgramming.playground/Pages/HouseRobber.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func rob(_ nums: [Int]) -> Int {
+    if nums.count == 0 { 0 }
+    if nums.count == 1 { nums[0] }
+    
+    var dp = Array(repeating: 0, count: nums.count)
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+    for i in 2..<nums.count {
+        dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+    }
+    return dp[nums.count - 1]
+}
+
+var a = [2,7,9,3,1]
+let v = rob(a)
