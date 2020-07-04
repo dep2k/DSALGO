58da54892e2503e4e1d6ff2fbc0043fef570c281
dep2k <deep.mobdev@gmail.com>
2020-07-04 18:18:21 +0530

    Reverse Linked List Recursively

--- Playgrounds/LinkedList.playground/Pages/ReverseLinkedList.xcplaygroundpage/Contents.swift
+++ Playgrounds/LinkedList.playground/Pages/ReverseLinkedList.xcplaygroundpage/Contents.swift
@@ -16,4 +16,17 @@ class Solution {
         current = prev
         return current
     }
+    
+    // 1->2->3->4->5
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        guard let head = head else { return nil}
+        
+        if (head.next == nil) {
+            return head
+        }
+        var reverseHead = reverseList(head.next)
+        head.next?.next = head
+        head.next = nil
+        return reverseHead
+    }
 }
