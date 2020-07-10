4079e6c76d65210d0dc00cf8e57dc4b555bbae84
Deep Arora <deep.mobdev@gmail.com>
2020-07-10 12:33:18 +0530

    Width Of Binary Tree

---
+++ Playgrounds/Tree.playground/Pages/WidthOfBinaryTree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,47 @@
+
+import Foundation
+
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+func widthOfBinaryTree(_ root: TreeNode?) -> Int {
+     if root == nil {
+         return 0
+     }
+     var queue = [(Int, TreeNode)]()
+     queue.append((0, root!))
+     var maxWidth = 0
+    
+     while !queue.isEmpty {
+         let length = queue.count
+         let first = queue.first!.0, last = queue.last!.0
+         maxWidth = max(maxWidth, last - first + 1)
+         
+         for _ in 0..<length {
+             let curNode = queue.removeFirst()
+             let node = curNode.1
+             if let left = node.left {
+                 queue.append((2 * curNode.0, left))
+             }
+             if let right = node.right {
+                 queue.append((2 * curNode.0 + 1, right))
+             }
+         }
+        
+         if queue.count == 1 {
+             queue[0].0 = 0
+         }
+     }
+     return maxWidth
+ }
