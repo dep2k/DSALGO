8871cab2d60bbdbd9f08aaa6dc8f287053c58621
Deep Arora <deep.mobdev@gmail.com>
2020-07-13 21:51:47 +0530

    Queue Restructuring

--- Playgrounds/Array.playground/Pages/QueueRestructuringByHeight.xcplaygroundpage/Contents.swift
+++ Playgrounds/Array.playground/Pages/QueueRestructuringByHeight.xcplaygroundpage/Contents.swift
@@ -5,14 +5,20 @@ import Foundation
 var input = [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
 
 func reconstructQueue(_ people: [[Int]]) -> [[Int]] {
-    let sorted = people.sorted { (a, b) -> Bool in
-        if (a[0] != b[0]) {
-            return b[0] - a[0] > 0
+    var sorted = people.sorted { (a, b) -> Bool in
+        if (a[0] == b[0]) {
+            return a[1] < b[1]
         } else {
-            return a[1] - b[1] > 0
+            return a[0] > b[0]
         }
     }
-    return sorted
+    print(sorted)
+    var restructed  = [[Int]]()
+    for item in sorted {
+        restructed.insert(item, at: item[1])
+        print(restructed)
+    }
+    return restructed
 }
 
 let reconstructed = reconstructQueue(input)
